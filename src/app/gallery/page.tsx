0c2cf65507e98656d8961e7596f96c994a82b024
JoyOffiong <blaqkly@gmail.com>
2024-12-01 08:01:04 +0100

    gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,11 +1,47 @@
+"use client";
+
+import Image from "next/image";
 import React from "react";
+import Masonry from "react-masonry-css";
 
 type Props = {};
 
 function Gallery({}: Props) {
+  const images = [
+    { img: "/images/img1.jpeg" },
+    { img: "/images/img2.jpeg" },
+    { img: "/images/img3.jpeg" },
+    { img: "/images/img4.jpeg" },
+    { img: "/images/img5.jpeg" },
+    { img: "/images/img6.jpeg" },
+    { img: "/images/img7.jpeg" },
+  ];
+
   return (
     <div>
-      <p className="text-center text-24 font-bold py-10  ">Coming Soon</p>
+      <div className="text-center text-24 font-bold py-10 m-8 ">
+        <Masonry
+          breakpointCols={{ default: 4, 1100: 3, 700: 2, 500: 1 }}
+          className="flex"
+          columnClassName="masonry-column"
+        >
+          {images.map((props, index) => {
+            const { img } = props;
+            return (
+              <div key={index} className="overflow-hidden">
+                <div className="relative h-64 m-4">
+                  <Image
+                    src={img}
+                    alt="Gallery Image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </Masonry>
+      </div>
     </div>
   );
 }
