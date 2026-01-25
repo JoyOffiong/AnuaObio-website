2a7fd73b0b34c4f8d7ed02df9ee50477b27105c6
JoyOffiong <blaqkly@gmail.com>
2026-01-25 19:02:20 +0100

    fixes

--- src/app/homePageComponents/JoinUsToday.tsx
+++ src/app/homePageComponents/JoinUsToday.tsx
@@ -5,27 +5,28 @@ type Props = {};
 
 function JoinUsToday({}: Props) {
   return (
-    <div className="grid my-20 items-center grid-cols-1 space-x-24 md:gap-0  md:grid-cols-2">
+    <div className="grid my-20 items-center w-full space-x-24 md:gap-0 gap-y-6  md:grid-cols-2">
       <div
-        className="bg-cover bg-center rounded-tr-2xl rounded-br-2xl relative z-10 font-sans"
+        className="bg-cover bg-center md:rounded-tr-2xl md:rounded-br-2xl relative z-10 font-sans"
         style={{
           backgroundImage: "url('/images/churchpix6.jpeg')",
           height: "700px",
           objectFit: "cover",
         }}
       >
-        <div className="absolute inset-0 bg-[#f3bb44] rounded-tr-xl rounded-br-xl opacity-30"></div>
+        <div className="absolute inset-0 bg-[#f3bb44] md:rounded-tr-xl md:rounded-br-xl opacity-30"></div>
       </div>
 
-      <div className="space-y-6 md:px-10 px-4 ">
-        <p className="text-[#333333] font-bold text-center md:text-left text-5xl leading-tight">
+      <div className=" flex justify-center text-center md:text-left md:justify-end md:px-10 px-4 ">
+        <div className="space-y-6">
+  <p className="text-[#333333] font-bold text-center md:text-left text-5xl leading-tight">
           <span> Here at the </span>
           <br /> <span className="text-5xl">
             {" "}
             CHURCH OF CHRIST,
           </span> <br /> <span className="3xl"> Anua Obio, Uyo</span>
         </p>
-        <p className=" text-2xl text-justify leading-normal font-medium text-[#888888] md:w-10/12 text-wrap">
+        <p className=" text-2xl leading-normal font-medium text-[#888888] md:w-10/12 text-wrap">
           Our goal is to worship God in Spirit & Truth. Our worship is organized
           in order to offer God an acceptable worship each time we meet. Our
           items of worship includes: Singing, Praying, Teaching, Sermon,
@@ -41,6 +42,8 @@ function JoinUsToday({}: Props) {
             <ArrowRight size={18} />
           </button>
         </div>
+        </div>
+      
       </div>
     </div>
   );
