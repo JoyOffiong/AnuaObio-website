1af5c173ee465a7470f8bc4fa6326c9559cf75e6
JoyOffiong <blaqkly@gmail.com>
2026-01-29 07:32:50 +0100

    commit pushed

---
+++ src/app/pages/teaching/Life-at-the-crossroads/page.tsx
@@ -0,0 +1,169 @@
+import React from "react";
+
+import BackButton from "@/sharedComponents/backButton";
+
+type Props = {};
+
+function LifeAtTheCrossRoads({}: Props) {
+  return (
+    <div className="font-inter my-24 space-y-6 md:my-32 mx-6 md:mx-32 ">
+      <BackButton />
+
+      <div className="flex flex-col gap-y-6 justify-center items-center h-full">
+        <div className="text-b text-center md:w-2/3 z-20 w-full">
+          <p className="font-bold uppercase text-xl md:text-5xl">
+Life at the Crossroads         </p>
+        </div>
+        <div className="space-y-4 text-[#888888]">
+          <p>October 19th, 2025</p>
+          <p>By Min E.O.Akpan</p>
+        </div>
+      
+      
+      </div>
+
+      <div className="text-[#000000] space-y-4">
+        <div className="space-y-4 ">
+          <p> &quot; Enter by the narrow gates, for the gate is wide and the way is easy, that leads to destruction, and those who enter by it are many. 14 for the gate is narrow, and the way is hard that leads to life and those who find it are few&quot; (Mathew 7:13-14). </p>
+          
+        </div>
+         <div className="space-y-4 ">
+          <p> <b>Objective:</b>  At the end of this lesson, we should be able to contrast the two ways Jesus taught His disciples in verses 13 and 14 for each of the following: The gate, the way, the direction, and the number of people who travel.
+          <br />
+  <em>What makes the Narrow Way hard to find?</em> 
+ </p>
+          
+        </div>
+        <div className="space-y-4 ">
+          <p className="font-bold text-lg">A. Introduction</p>
+          <p>
+There is always a certain dramatic quality about life, for as it has been said, &quot;all life concentrates on man at the cross-road&quot;
+ In every action of life, man is confronted with a choice, and he can never evade the choice, because he can never stand still. He must always take one way or the other (Mathew 7:13-14). In these verses, Jesus used the figure of a path to describe the choice between two ways of life. Leading to two very different destinies. Which of the two paths we choose to walk is left entirely up to us.
+          </p>
+          
+        </div>
+        <div className="space-y-2">
+          <p className="font-bold text-lg">B. Discussion</p>
+        <p> Choosing the right path (Matt.7:13-14): God has always made an effort to bring people to make that choice. There is always an option, so there is always a choice. The ultimate choice is what God is most concerned. As the Israelites were preparing to enter Canaan, Moses assembled the people to give them a parting message and to inform them he would not be going with them into the Promised Land (Deut. 31:2). In this last address he made and impassion bleed for them to be faithful to God (Deut. 30:19). He said "I have set before you life and death, blessing and cursing: therefore choose life, that both thousand they seed may live" (Deut30:19): he called to make a decision shortly before his dead. Joshua likewise challenged Israel, saying choose you this day whom ye will serve, whether the God which your father served that was on the other side of the flood, or the gods of the Amorites, in whose land ye dwell: but as for me and my house, we will serve the Lord" (Joshua 24:15).</p>
+<p> Jeremiah reminded the people of Judah of the choice before them: he had to remain in Jerusalem and be destroyed along with it, or to flee from it as the judgment of God was approaching. He wrote "thus saith the Lord, behold I said before you the way of life and the way of death" (Jer21:8).</p>
+<p>From the beginning, God has made it plain that people really have only two options. We can choose to obey God or disobey him. There are not many ways to God, there's only one way.</p>
+<p>Enter through the narrow gate in his discussion of the parable of the closed door. Jesus said, strive to enter in at the strait gate; for many, I say unto you, will seek to enter in, and shall not be able" (Lk. 13:24). The contrast of verses 13 and 14 is obvious.</p>
+<p>There are two ways and two ways that each of us can go; the narrow way leads to life, and the broad way leads to destruction. The narrow way is the difficult path, and only a "few&quot;are willing to make the sacrifice necessary to travel on it. The broad way is the easy way, the popular way, chosen by many. Let us examine the difference between the two ways. It is the difference between the art and the easy way. There is never any easy way to greatness; greatness is always the product of toil. There has been no other way to greatness than the way of toil, and anything else which promises such a way is a delusion and a snare</p>
+
+<p>It is different between the disciplined and the undisciplined; nothing is ever achieved without discipline. Many have been ruined because they have abandoned this plane and let themselves grow slack.
+ Which route are you talking about, Matthew 7:13-14? We find four contrasts.
+</p>
+<p>Two ways- two ways are available to the traveller. One is Broadway. The broad way is spacious and roomy.  There are no boundaries. You can be what you want to be, do what you want to do.  There is a narrow way- unlike Broadway, it is not spacious. Jesus never depicted the road to life as an easy pathway.</p>
+
+<p>
+    <em>Two destinations </em>
+    <br />
+    We have four contrasts, but only two option is either the narrow way that leads to life, or the broad way that leads to destruction.
+</p>
+<p>John R. Spott wrote. There are, according to Jesus, only two ways, hard and easy (there is no middle way), entered by to gate, broad and narrow (there is no other gate), trodden by two crowds, large and small (there is no neutral group), ending in two destinations, destruction and life (there is no third alternative)</p>
+
+<div className="space-y-1">
+    <b> The narrow gate</b>
+
+    <em>You must enter - The command from Jesus</em>
+    <p> You must enter the narrow gate. Jesus said that there is a wide gate, but he did not tell us to enter it, because it leads to destruction if you are going to be in the kingdom, you have to go through the narrow gates. You cannot just admire it.</p>
+    <p> The basis of salvation, Jesus said, &quot;I came that they may have life and have it abundantly &quot;(John 10:10b). If spiritual death is separation from God, then spiritual life is union with God, fellowship with him. On the narrow road, fellowship with God and Christ is begun in heaven; it is perfected. </p>
+
+    <p>B Act 4:12 says, &quot;Neither is there salvation in any other, for there is no other name or under heaven given among men, whereby we must be saved&quot; Jesus said, &quot;I am the bread of the life&quot; (John6:35) and &quot;I am the way, the truth and life&quot; ( John 14:6). In John 10 says that he is &quot;the door of the sheep (John 10:7) and that he that entered not by the door but climbeth up some other way, the same is a thief and robber&quot; Christ is the only way to salvation.</p>
+
+   
+</div>
+
+<div className="space-y-1">
+<p><b> The way is narrow. There are no alternatives</b></p>
+<p>You must enter by an act of faith. You have to enter on God&apos;s terms through God&apos;s prescribed gates.
+ Christ is that gates (John10:9), He is the only way.
+</p>
+<p>God has the right to determine the basis of salvation, and he has determined that it is through Christ alone.
+ What makes the narrow way hard to find is the existence of numerous false teachers who have their own formulas for man&apos;s welfare and who cry aloud in our own time. Choose the right way and be saved.
+</p>
+</div>
+        </div>
+        <div className="space-y-2">
+          <p className="font-bold text-lg">C. Why do people turn to spiritualism / engage in it?</p>
+
+         <ul className="list-disc list-inside space-y-2">
+  <li>Fear – 1 Sam. 28:5</li>
+  <li>Calamity</li>
+  <li>War</li>
+  <li>Sickness</li>
+  <li>Deaths</li>
+  <li>Peer pressure</li>
+  <li>Money</li>
+  <li>Power</li>
+  <li>Favour</li>
+  <li>Success at exams</li>
+  <li>Winning elections and contracts</li>
+  <li>Protection</li>
+</ul>
+        </div>
+
+        <div className="space-y-2">
+          <p className="font-bold text-lg">D. Warnings / Consequences</p>
+
+         <ul className="list-disc list-inside space-y-2">
+  <li>Death (cut off from God&apos;s people)</li>
+  <li>Abominable before God and man</li>
+  <li>Provokes God&apos;s anger – 2 Chron. 33:6; 2 Kings 17:17</li>
+  <li>Defilement</li>
+  <li>Shame / Disgrace</li>
+  <li>Turns away God&apos;s face / favour</li>
+  <li>Do not imitate them – Deut. 18:9, 12</li>
+  <li>
+    Put them away
+    <ul className="list-inside space-y-2">
+      <li> - Saul put them away – 1 Sam. 28:3</li>
+      <li> - Josiah put them away – 2 Kings 23:24</li>
+    </ul>
+  </li>
+  <li>Do not consult the dead for the living – Isa. 8:19</li>
+  <li>Flee from all appearances of evil</li>
+  <li>Sorceries fail / disappoint – Isa. 47:9</li>
+  <li>Such will not inherit the kingdom of God – Gal. 5:20</li>
+  <li>Their place will be in hellfire – Rev. 21:8</li>
+  <li>
+    God has always condemned spiritualism and related practices
+    <ul className="list-inside space-y-2">
+      <li> - 1 Sam. 28:5 – Saul and the witch of Endor</li>
+      <li>
+       - 2 Kings 21:1–6 – King Manasseh (he did more evil than the nations
+        God destroyed before them)
+      </li>
+    </ul>
+  </li>
+</ul>
+
+        </div>
+
+         <div className="space-y-2">
+          <p className="font-bold text-lg">E. Beware of modern-day practices/ items bordering on spiritism.</p>
+
+         <ul className="list-disc list-inside space-y-2">
+  <li>Yahoo, yahoo</li>
+  <li>Money rituals</li>
+  <li> Casting of spells</li>
+  <li>Witchcraft</li>
+  <li>Idols/ idolatry</li>
+  <li>Rings, bands, bracelets, chains, etc</li>
+  <li>Tattoos</li>
+    <li>Assignments for husband, job, children (pregnancy), good luck, overcoming enemies, seeing the future, winning the lottery, contracts, elections, getting more customers, favour from the boss, etc.</li>
+    <li>Cultism/Masquerades</li>
+</ul>
+        </div>
+ <div className="space-y-2">
+          <p className="font-bold text-lg">F. Conclusion</p>
+<p>God&apos;s children should consult God and trust Him at all times, particularly in times of distress. Only God knows all secrets. Secret things belong to God. He reveals what he wants to reveal to us. Dan. 2:27. Do not turn to mediums and soothsayers. Turn to God. God bless you. </p>
+        </div>
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default LifeAtTheCrossRoads;
