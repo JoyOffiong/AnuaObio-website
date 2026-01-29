69bf3e5743caf1eb5c7dc615787f2fcbb68e4113
JoyOffiong <blaqkly@gmail.com>
2026-01-29 07:10:53 +0100

    update

--- src/app/pages/teaching/Jesus-and-the-mosaic-law/page.tsx
+++ src/app/pages/teaching/Jesus-and-the-mosaic-law/page.tsx
@@ -30,15 +30,15 @@ function JesusAndTheMosaicLaw({}: Props) {
           <p className="font-bold text-lg">Lesson Outline</p>
           
           <ul>
-            <li>1. Introduction</li>
-            <li>2. Jesus&apos; relation to the Law of Moses (matt. 5:17-20)
+            <li>i. Introduction</li>
+            <li>ii. Jesus&apos; relation to the Law of Moses (matt. 5:17-20)
 
 </li>
-            <li>3. Teaching against murder (matt. 5:21-26)</li>
-            <li>4. Teaching against adultery and divorce (Matt. 5:27-32)</li>
-            <li>5. Teaching against oaths and retaliation (matt. 5:33-42)</li>
-            <li>6. The principle of love (matt. 5:43-48)</li>
-            <li>7. Application and conclusion</li>
+            <li>iii. Teaching against murder (matt. 5:21-26)</li>
+            <li>iv. Teaching against adultery and divorce (Matt. 5:27-32)</li>
+            <li>v. Teaching against oaths and retaliation (matt. 5:33-42)</li>
+            <li>vi. The principle of love (matt. 5:43-48)</li>
+            <li>vii. Application and conclusion</li>
           </ul>
         </div>
         <div className="space-y-2 ">
@@ -62,7 +62,7 @@ function JesusAndTheMosaicLaw({}: Props) {
           <p className="font-bold text-lg">B. The new standard.</p>
            <p className="font-bold text-md"> <em>I.  Teaching against murder (Matt. 5:21-26).</em> </p>
           <p>
-          The anger that is not allowed in this first example of the new standard that Jesus set. The old law said, "You shall not kill" (Exo. 20:13), but Jesus said that even being angry at a brother is wrong. Not only should you not hit a man, but you shouldn't even want to hit him or have bad feelings towards him in your heart. Jesus says that all selfish anger is wrong. The Bible plainly says that anger is wrong. James said, "The anger of man does not work the righteousness of God."(James 1:20). Paul told his people to stop being angry (Col3:8). Jesus forbade the anger that broods, the anger that won't go away, the anger that won't be calmed down, and the anger that wants revenge. Jesus goes on to talk about two times when anger leads to rude words. (Psa109:1-3, Deu19:16-18) (Deut19:11-12). It is also wrong for a man to speak in anger. Jesus says that the person who ruined his brother's name and reputation is the one who will be judged most harshly, by the fire of Gehenna.
+          The anger that is not allowed in this first example of the new standard that Jesus set. The old law said, &quot;You shall not kill&quot;(Exo. 20:13), but Jesus said that even being angry at a brother is wrong. Not only should you not hit a man, but you shouldn&apos;t even want to hit him or have bad feelings towards him in your heart. Jesus says that all selfish anger is wrong. The Bible plainly says that anger is wrong. James said, &quot;The anger of man does not work the righteousness of God.&quot;(James 1:20). Paul told his people to stop being angry (Col3:8). Jesus forbade the anger that broods, the anger that won&apos;t go away, the anger that won&apos;t be calmed down, and the anger that wants revenge. Jesus goes on to talk about two times when anger leads to rude words. (Psa109:1-3, Deu19:16-18) (Deut19:11-12). It is also wrong for a man to speak in anger. Jesus says that the person who ruined his brother&apos;s name and reputation is the one who will be judged most harshly, by the fire of Gehenna.
        
           </p>
         </div>
@@ -71,8 +71,8 @@ function JesusAndTheMosaicLaw({}: Props) {
            II. Teaching against adultery and lust (Matt. 5:31-32): {" "}
           </p>
           <p>
-           The forbidden desire you have heard that it been said: "You must not commit adultery. 
- Here is Jesus' second example of a new standard. The law laid it down you shall not commit adultery (Exo. 20:14). So seriously a view did the Jewish teacher take of adultery that the guilty parties could be punished by nothing less than death (Lev. 20:10); but once again Jesus lays it down that not only the forbidden action but also the forbidden thought is guilty in the sight of God. We must understand what Jesus is saying here. He is not speaking of the natural, normal desire which is part of human instinct and human nature. According to the literal meaning of the Greek, the man who is condemned is the man who looks at a woman with the deliberate intention of lusting after her. The man who is condemned is the man who deliberately uses his eyes to awaken his lust, the man who looks in such a way that passion is awakened and desire deliberately stimulated. Verses 29 and 30 provide a cure.
+           The forbidden desire you have heard that it been said: &quot;You must not commit adultery. 
+ Here is Jesus&apos; second example of a new standard. The law laid it down you shall not commit adultery (Exo. 20:14). So seriously a view did the Jewish teacher take of adultery that the guilty parties could be punished by nothing less than death (Lev. 20:10); but once again Jesus lays it down that not only the forbidden action but also the forbidden thought is guilty in the sight of God. We must understand what Jesus is saying here. He is not speaking of the natural, normal desire which is part of human instinct and human nature. According to the literal meaning of the Greek, the man who is condemned is the man who looks at a woman with the deliberate intention of lusting after her. The man who is condemned is the man who deliberately uses his eyes to awaken his lust, the man who looks in such a way that passion is awakened and desire deliberately stimulated. Verses 29 and 30 provide a cure.
 
           </p>
          
@@ -84,43 +84,34 @@ function JesusAndTheMosaicLaw({}: Props) {
           </p>
          <p>In Matthew 5:33-37, Jesus teaches against taking oaths and retaliation, emphasizing honesty and non-violent responses. He instructs:</p>
          <ul className="list-disc list-inside space-y-2">
-
+<li>Don&apos;t swear by heaven, earth, or Jerusalem (v34-35)</li>
+<li>Say &quot;yes&quot; or &quot;no&quot; plainly (v37)</li>
+<li>Don&apos;t retaliate with evil (v38-42)</li>
          </ul>
          
-       
+         <p>Jesus&apos; message:</p>
+         <ul className="list-disc list-inside space-y-2">
+<li>Promotes simplicity and truthfulness in speech</li>
+<li>Discourages using oaths to manipulate or deceive</li>
+<li>Encourages non-resistance and turning the other cheek</li>
+         </ul>
+
+         <p>This teaching is about integrity, trust, and responding to evil with kindness. </p>
+         <p>Revenge (Matt. 5:43-48): 
+ This teaching emphasizes Christian love. Meaning of it, and the reason for it (vs45-48)
+
+</p>
         </div>
         <div className="space-y-2">
-          <p className="font-bold text-lg">C. What Will Godly Knowledge Do for a Man Who Possesses It? </p>
+          <p className="font-bold text-lg">Conclusion: </p>
           
-          <ul>
-            <li>1. It will preserve him (v. 6); if you do not forsake it, it preserves you.  </li>
-            <li>2. When you love it, it keeps you (v. 6).  </li>
-            <li>3. When you exalt it, it promotes you (v. 8).  </li>
-            <li>4. When you embrace it, it brings you honour (v. 8).  </li>
-            <li>5. It will even decorate you with an ornament of grace and deliver to you a crown of glory (Proverbs 4:5-9).  </li>
-          </ul>
+          <p></p>
           <p>
-            {" "}
-            Knowledge is God&apos;s instrument of deliverance. The proverbs further identify the advantages of being wise. A person who uses his intelligence, along with guidance received from trusting in the Lord, holds a significant advantage, even when facing overwhelming odds, like a fortified city. In Ecclesiastes, Solomon notes that wisdom is better than strength and weapons (Ecclesiastes 9:16-18).  
+           These teaching of Jesus were to make his disciples pure, holy, true doers of good, that ye may be sons of your father who is in heaven.  
           </p>
+ 
+        </div>
         
-          <p>
-           We read in Proverbs 21:22, &quot;A wise man scaleth the city of the mighty, and casteth down the strength of the confidence thereof.&quot; The city represents a stronghold, heavily fortified with walls and towers. Verse 2:2a concludes the story, where he succeeds in bringing down this stronghold. This illustrates that a man who relies on God for wisdom is stronger than any man-made fortification. The inhabitants of the city place their trust in their walls and soldiers for various reasons; however, they would have fared better by trusting in the Lord, who can protect His people without walls or soldiers. This is what the wise man does, leading to his victory. Everyone who trusts in the Lord and His wisdom will be equally blessed. We read in Proverbs 24:14, &quot;Know that wisdom is this for your soul; if you find it, then there will be a future, and your hope will not be cut off.&quot; If a person discovers wisdom, it will provide a favourable future for him, and his hope will remain intact.
-          </p>
-
-          <p>Throughout Proverbs, Solomon encourages us to be wise. A wise son heeds the admonition to seek wisdom, allowing the father to see his son making choices and decisions based on sound information found in God&apos;s word. When a father has a son who lives according to this standard, he will rejoice greatly. We are reminded that these proverbs are designed to help individuals become wise and avoid the foolish mistakes made by the ungodly.</p>
-        </div>
-        <div className="space-y-2 ">
-          <p className="font-bold text-lg">
-          Conclusion
-          
-          </p>
-           <p>
-            &quot;Be very careful, then, how you live—not as unwise but as wise, making the most of every opportunity, because the days are evil. Therefore do not be foolish, but understand what the Lord&apos;s will is.  &quot;
-           Ephesians 5:15-17.
-          </p>
-        </div>
-       
       </div>
     </div>
   );
