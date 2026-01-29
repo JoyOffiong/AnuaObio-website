d280ce28c57d3ba73536c95ad017e3ad4bb6c6ad
JoyOffiong <blaqkly@gmail.com>
2026-01-29 05:04:06 +0100

    new updates

---
+++ src/app/pages/teaching/Jesus-and-the-mosaic-law/page.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+
+import BackButton from "@/sharedComponents/backButton";
+
+type Props = {};
+
+function JesusAndTheMosaicLaw({}: Props) {
+  return (
+    <div className="font-inter my-24 space-y-6 md:my-32 mx-6 md:mx-32 ">
+      <BackButton />
+
+      <div className="flex flex-col gap-y-6 justify-center items-center h-full">
+        <div className="text-b text-center md:w-2/3 z-20 w-full">
+          <p className="font-bold uppercase text-xl md:text-5xl">
+            Jesus and the Mosaic Law
+          </p>
+        </div>
+        <div className="space-y-4 text-[#888888]">
+          <p>October 12th, 2025</p>
+          <p>By Min. E.O. Akpan</p>
+        </div>
+
+        <div className="font-semibold">
+          <em>Matt. 5:17-48</em>
+        </div>
+      </div>
+
+      <div className="text-[#000000] space-y-4">
+         <div className="space-y-2">
+          <p className="font-bold text-lg">Lesson Outline</p>
+          
+          <ul>
+            <li>1. Introduction</li>
+            <li>2. Jesus&apos; relation to the Law of Moses (matt. 5:17-20)
+
+</li>
+            <li>3. Teaching against murder (matt. 5:21-26)</li>
+            <li>4. Teaching against adultery and divorce (Matt. 5:27-32)</li>
+            <li>5. Teaching against oaths and retaliation (matt. 5:33-42)</li>
+            <li>6. The principle of love (matt. 5:43-48)</li>
+            <li>7. Application and conclusion</li>
+          </ul>
+        </div>
+        <div className="space-y-2 ">
+          <p className="font-bold text-lg">A. Introduction</p>
+          <p>
+            • Jesus said &quot;You have heard….&quot; (Matt. 5:17-48, Luke 6:27-30, 32,36). The longest section of the Sermon on the Mount in Matthew 5:17-48, which contrasts the law of Moses and related to man-made traditions with Jesus teaching, it was essential that Christ&apos;s disciples have a clear understanding of his relationship to the law and also his attitude toward the multiplicity of traditions that men had attached to the law. 
+          </p>
+           <p>
+            • The introductory statement is very important. Jesus began, “Think not that I am come to destroy the law, or the prophet: I am not come to destroy but to fulfill. For verily I say unto you, till heaven and earth pass, one jot or one little shall in no wise pass from the law, till all be fulfilled&quot; (Matt. 5:17-18). The fact that Jesus came not &quot;to destroy but fulfill&quot; the law has led some to believe that we are still under the Old Testament today. This interpretation of Christ&apos;s words would make him contradict the plan of teaching of his inspired apostles. Paul concurs as he reminds the church at Rome. For Christ is the end of the law (law of Moses) for righteousness to everyone who believes&quot; (Rev. 10:4). Paul wrote about Jesus, &quot;Abolishing in his flesh the enmity, even the law of commandments contained is ordinance; for to make in himself of twain one new man&quot; (Eph. 2:15). The apostle also spoke about &quot;the handwriting of ordinances that was against us.&quot; That is what Jesus said in Matthew: &quot;He took it out of the way, nailing it to the cross&quot; (Col. 3:14). You can understand Matt. 5:17-18 better if you remember that the Old Testament was a deal between God and the Jews. (Deut. 4:13, 5:2–3).
+          </p>
+          <p>
+            • They, alone, were God&apos;s covenant people, which is to say that anyone outside of the covenant relationship with the Lord God was an alien or stranger in that they were uncircumcised (Gen17:9-14). Think of the Old Testament as a contract between God and Israel. Jesus did not come to destroy; here the word &quot;destroy&quot; means loosen down, to dissolve, or undo. Jesus did not come to abrogate or set aside the law and the prophets, but to fulfill. He fulfilled the prophecies, he kept the demands of the law, and accomplished the law&quot;s purposes (Gal. 3:19, 5:14).  
+          </p>
+             <p>
+            • At the time Jesus preached on the mount, the law was still in force. As long as that was the case, throughout his life, Jesus kept the Law of Moses, but not the traditions of the Pharisees and Sadducees, who had adulterated the Law given to them by God through Moses. They had perverted the law by their man-made traditions, which was one of the root causes of confrontation with Jesus and why he approved.  
+          </p>
+          <p>• Christ taught his disciples to honour the demands of the Law (Matt. 5:19-20). His objection was not to the law, but to Jewish misinterpretation of the law. Jesus kept the very letter of the law and made His position clear in the Sermon on the Mount, when declared, “Do not think that I came to destroy the law or the prophet....” (Matt. 5:17-20). In the verses that follow, Jesus explained several commandments of the law to include the attitude of the heart essential to keeping the law. He also contrasted his way with what the people had taught by the scribe and Pharisees.</p>
+        </div>
+        
+        <div className="space-y-2">
+          <p className="font-bold text-lg">B. The new standard.</p>
+           <p className="font-bold text-md">I. Exhortation to Embrace Wisdom:</p>
+          <p>
+           Children who receive proper instruction from faithful parents are promised a long life. The wise man said, &quot;And the years of your life shall be many&quot; (4:10). Notice the signs of love in the parents&rsquo; advice in Proverbs 4:1-4. The father continues to emphasise the importance of gaining wisdom and understanding, pleading with the child to listen, hear, and remember. &quot;Hear, my children, the instruction of a father, and give attention to know understanding. For I give you good teaching; do not forsake my law&quot; (Proverbs 4:2). Parents desire their children to thrive with wisdom, which increases the chances of success. 
+            <br />
+         &quot;Hear, O my son, and receive my sayings, and the years of your life will be many&quot; (Proverbs 4:10). This proverb speaks of life: survival, avoiding problems, and overcoming the obstacles along the path. &quot;Take firm hold of instruction. Do not let her go; keep her, for she is your life&quot; (Proverbs 4:13).
+          </p>
+        </div>
+        <div className="space-y-2">
+          <p className="font-bold text-md">
+           II. Supremacy of Wisdom:.{" "}
+          </p>
+          <p>
+           &quot;Get wisdom. Get understanding. Do not forget it, nor turn away from the words of my mouth. Do not forsake her, and she will preserve you. Love her, and she will keep you. Wisdom is the principal thing; therefore, get wisdom. And with all your acquiring, get understanding. Exalt her, and she will promote you; she will bring you honour when you embrace her. She will give you a crown of glory&quot; (Proverbs 4:5-9). 
+          </p>
+         
+          <p>Wisdom, knowledge, and understanding are closely connected; they are the offspring of the same parents. It takes wisdom to pursue knowledge, and the ability to apply knowledge correctly is what we call understanding. The proverbs state that wisdom is supreme and the first thing one should seek. Knowledge of God and His words is emphasised throughout the Bible. The moment you gain insight into a matter is the moment you begin to escape the path of ignorance and bondage. A wise person is a knowledgeable person.
+          </p>
+          <p>Verses 4-7 describe what wisdom is and emphasise the need for loyalty to it.  </p>
+        </div>
+        <div className="space-y-2">
+          <p className="font-bold text-lg">C. What Will Godly Knowledge Do for a Man Who Possesses It? </p>
+          
+          <ul>
+            <li>1. It will preserve him (v. 6); if you do not forsake it, it preserves you.  </li>
+            <li>2. When you love it, it keeps you (v. 6).  </li>
+            <li>3. When you exalt it, it promotes you (v. 8).  </li>
+            <li>4. When you embrace it, it brings you honour (v. 8).  </li>
+            <li>5. It will even decorate you with an ornament of grace and deliver to you a crown of glory (Proverbs 4:5-9).  </li>
+          </ul>
+          <p>
+            {" "}
+            Knowledge is God&apos;s instrument of deliverance. The proverbs further identify the advantages of being wise. A person who uses his intelligence, along with guidance received from trusting in the Lord, holds a significant advantage, even when facing overwhelming odds, like a fortified city. In Ecclesiastes, Solomon notes that wisdom is better than strength and weapons (Ecclesiastes 9:16-18).  
+          </p>
+        
+          <p>
+           We read in Proverbs 21:22, &quot;A wise man scaleth the city of the mighty, and casteth down the strength of the confidence thereof.&quot; The city represents a stronghold, heavily fortified with walls and towers. Verse 2:2a concludes the story, where he succeeds in bringing down this stronghold. This illustrates that a man who relies on God for wisdom is stronger than any man-made fortification. The inhabitants of the city place their trust in their walls and soldiers for various reasons; however, they would have fared better by trusting in the Lord, who can protect His people without walls or soldiers. This is what the wise man does, leading to his victory. Everyone who trusts in the Lord and His wisdom will be equally blessed. We read in Proverbs 24:14, &quot;Know that wisdom is this for your soul; if you find it, then there will be a future, and your hope will not be cut off.&quot; If a person discovers wisdom, it will provide a favourable future for him, and his hope will remain intact.
+          </p>
+
+          <p>Throughout Proverbs, Solomon encourages us to be wise. A wise son heeds the admonition to seek wisdom, allowing the father to see his son making choices and decisions based on sound information found in God&apos;s word. When a father has a son who lives according to this standard, he will rejoice greatly. We are reminded that these proverbs are designed to help individuals become wise and avoid the foolish mistakes made by the ungodly.</p>
+        </div>
+        <div className="space-y-2 ">
+          <p className="font-bold text-lg">
+          Conclusion
+          
+          </p>
+           <p>
+            &quot;Be very careful, then, how you live—not as unwise but as wise, making the most of every opportunity, because the days are evil. Therefore do not be foolish, but understand what the Lord&apos;s will is.  &quot;
+           Ephesians 5:15-17.
+          </p>
+        </div>
+       
+      </div>
+    </div>
+  );
+}
+
+export default JesusAndTheMosaicLaw;
